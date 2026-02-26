1c0439d709faa0d4e1a49a08b09fb10c33405aec
Ohad Raza <ohadraza345@gmail.com>
2026-02-27 04:07:00 +0500

    Initial commit

---
+++ numbers/Palindrome_number.py
@@ -0,0 +1,15 @@
+def isPalindrome(x):
+        last_digit = 0
+        original_number = x
+        reverse = 0
+        while x > 0:
+            last_digit = x % 10
+            reverse = reverse * 10 + last_digit
+            x //= 10
+        if original_number == reverse:
+            return True
+        else:
+            return False
+
+print(isPalindrome(121))
+print(isPalindrome(122))
