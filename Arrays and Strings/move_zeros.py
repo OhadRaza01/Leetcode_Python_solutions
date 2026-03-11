84bf46cd98787528bc2533d1467d42bc71bc91a9
Ohad Raza <ohadraza345@gmail.com>
2026-03-11 22:09:05 +0500

    Create move_zeros.py

---
+++ Arrays and Strings/move_zeros.py
@@ -0,0 +1,11 @@
+
+def zeros(nums):
+    j = 0 
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[i] , nums[j] = nums[j] , nums[i]
+            j += 1
+    
+    return nums
+
+print(zeros([0,1,0,3,12]))    
